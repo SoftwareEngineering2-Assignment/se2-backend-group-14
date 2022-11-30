ad5bbbfdd51220df12026af1e06f99212d6c9197
Ioannis Tziralis <74872672+intziral@users.noreply.github.com>
2022-11-30 14:22:46 +0200

    Update schemas.js

--- src/utilities/validation/schemas.js
+++ src/utilities/validation/schemas.js
@@ -3,34 +3,53 @@ const {isNil} = require('ramda');
 const yup = require('yup');
 const {min} = require('./constants');
 
+// Email input validation
 const email = yup
   .string()
   .lowercase()
   .trim()
   .email();
 
+//  Username input validation
 const username = yup
   .string()
   .trim();
 
+//  Password input validation
 const password = yup
   .string()
   .trim()
   .min(min);
 
+/* 
+* Forgotten password form request
+* Password input validation
+*/
 const request = yup.object().shape({username: username.required()});
 
+/* 
+* Sign in form validator
+* Checks username and password input
+*/
 const authenticate = yup.object().shape({
   username: username.required(),
   password: password.required()
 });
 
+/* 
+* Sign up form validator
+* Checks username, password and email input
+*/
 const register = yup.object().shape({
   email: email.required(),
   password: password.required(),
   username: username.required()
 });
 
+/* 
+* Update form validator
+* Checks username and password input
+*/
 const update = yup.object().shape({
   username,
   password
@@ -39,6 +58,10 @@ const update = yup.object().shape({
   test: ({username: u, password: p}) => !(isNil(u) && isNil(p))
 });
 
+/* 
+* Change password form validator
+* Checks password input
+*/
 const change = yup.object().shape({password: password.required()});
 
 module.exports = {
